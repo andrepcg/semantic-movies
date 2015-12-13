7ba4f111174b5cef241608fc0390574715f2afef
André Perdigão <andrepcg@gmail.com>
2015-12-13 23:57:41 +0000

    ?

--- server.py
+++ server.py
@@ -2,8 +2,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+	return render_template('home.html', name=name)
+
+@app.route("/movies")
+def movie(imdb_id=None):
+	# filmes por ano
+	return render_template('movie.html', movies=)
+
+@app.route("/movies/<imdb_id>")
+def movie(imdb_id=None):
+	# procurar filme por imdb_id
+	return render_template('movie.html', movie=)
+
+@app.route("/person/<imdb_id>")
+def person(imdb_id=None):
+	# procurar pessoa por imdb_id
+	return render_template('person.html', person=)
 
 if __name__ == "__main__":
-    app.run()
+	app.run(debug=True)
